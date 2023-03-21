17c73d2f843e0457d4bf8628593203dfd3dafa0a
bethke2 <eliot.bethke@gmail.com>
2023-03-21 10:03:50 -0500

    n_similarity provides better results

--- text_analysis/gensim_acm_analysis.py
+++ text_analysis/gensim_acm_analysis.py
@@ -4,7 +4,7 @@ import re
 import string
 import numpy as np
 
-NUM_MATCHES = 5
+NUM_MATCHES = 10
 
 pat = re.compile('()')
 
@@ -18,15 +18,24 @@ word_vect = KeyedVectors.load_word2vec_format("SO_vectors_200.bin", binary=True)
 
 CS124 = "Basic concepts in computing and fundamental techniques for solving computational problems. Intended as a first course for computer science majors and others with a deep interest in computing."
 CS124 = CS124.translate(str.maketrans('', '', string.punctuation))
+
+CS227 = ("Introduction to elementary concepts in algorithms and classical data structures with a focus on their applications in Data Science.", "Topics include algorithm analysis (ex: Big-O notation), elementary data structures (ex: lists, stacks, queues, trees, and graphs), basics of discrete algorithm design principles (ex: greedy, divide and conquer, dynamic programming), and discussion of discrete and continuous optimization.")
+CS227 = [C.translate(str.maketrans('', '', string.punctuation)) for C in CS227]
+# print(CS227)
+
 words = data['SEARCH'].to_list()
 words = [pat.sub('', w) for w in words]
 highest_rel = np.zeros(len(words))
 
-for i,w in enumerate(words):
-	similarity = word_vect.wmdistance(w, CS124)
-	highest_rel[i] = similarity		
-	# print(f"CS124 vs {w}: {similarity}")
+for sentence in CS227:
+	for i,w in enumerate(words):
+		# similarity = word_vect.wmdistance(w.lower().split(), sentence.lower().split())
+		similarity = word_vect.n_similarity(w.lower().split(), sentence.lower().split())
+		if similarity == np.inf:
+			similarity = 1
+		highest_rel[i] = similarity		
+		# print(f"CS124 vs {w}: {similarity}")
 
-highest_matches = np.argpartition(-highest_rel, NUM_MATCHES)[:NUM_MATCHES]
-print(np.array(words)[highest_matches], np.array(highest_rel)[highest_matches])
+	highest_matches = np.argpartition(-highest_rel, NUM_MATCHES)[:NUM_MATCHES]
+	print(np.array(words)[highest_matches], np.array(highest_rel)[highest_matches])
 
